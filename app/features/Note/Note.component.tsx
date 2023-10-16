011714a5aa8e45669b382b4dff8f2f4d86738a61
Alejo <alejoivanrojas@gmail.com>
2023-10-15 22:58:09 -0300

    add useRef support

--- app/features/Note/Note.component.tsx
+++ app/features/Note/Note.component.tsx
@@ -1,14 +1,15 @@
 import { ReactProps } from "@/app/types";
-import { FC } from "react";
+import { FC, useRef } from "react";
 import Draggable from "react-draggable";
 
 
 
 const Note: FC<ReactProps> = ({ children }) => {
+    const nodeRef = useRef(null);
 
     return (
-        <Draggable>
-            <div   className="text-gray-800 text-xl h-96 w-96 m-10 cursor-pointer p-4 bg-yellow-100 shadow-xl relative overflow-hidden">
+        <Draggable nodeRef={nodeRef}>
+            <div ref={nodeRef}  className="text-gray-800 text-xl h-96 w-96 m-10 cursor-pointer p-4 bg-yellow-100 shadow-xl relative overflow-hidden">
                 <div className=" absolute top-0 left-0 w-4 h-8 bg-yellow-200 transform rotate-45 -translate-x-2 -translate-y-2"></div>
                 {children}
             </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 'use client'
-import Note from './features/Note'
+import Note from "./features/Note"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen bg-radial-pattern flex-col items-center justify-start p-24">
+    <main className="flex min-h-screen bg-radial-pattern justify-start p-24">
       <Note>
         <small>Just trying 🤷‍♀️🧵</small>
       </Note>
