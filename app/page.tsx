f482d020d094ed0a719e979e215d26318c441631
Alejo <alejoivanrojas@gmail.com>
2023-10-17 13:58:40 -0300

    fix home page link

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ import Link from "next/link"
 const Home = () => {
   return (
     <main className="flex min-w-screen min-h-screen xs:p-2 md:p-10">
-      <Note cancel=".footer .a" properties="bg-yellow-100" cornerProperties="bg-yellow-200">
+      <Note cancel=".footer" properties="bg-yellow-100" cornerProperties="bg-yellow-200">
         <header className="cursor-pointer">
           <p>Hey guys,</p>
           <p>This is my last-minute portfolio. I&apos;ve been busy with some serious procrastination stuff, so this is what I&apos;ve got </p>
         </header>
-        <small className="small md:text-xl xs:text-base cursor-default ">Anyway, <a className="a text-blue-500" href="mailto:alejoivanrojas@gmail.com">email</a> me if you have any suggestions</small>
+        <small className="small md:text-xl xs:text-base cursor-default ">Anyway, <a className="text-blue-500" href="mailto:alejoivanrojas@gmail.com">email</a> me if you have any suggestions</small>
        
        <footer className="footer mt-6">
           <ul className="leading-[2.5rem]">
